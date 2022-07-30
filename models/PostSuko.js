ac2efdb15c2ac3533c2b690c9c6c54ea2e0c1a84
tareita <tareitaa@outlook.com>
2022-07-30 15:33:24 +0100

    likes schema created

--- models/PostSuko.js
+++ models/PostSuko.js
@@ -1,3 +1,14 @@
 const mongoose = require("mongoose");
 
-const sukoSchema = new mongoose.Schema({});
+const sukoSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Post",
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("PostSuko", sukoSchema);
