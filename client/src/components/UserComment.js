80f9773b07d3c251a7757a133220f7b28c655fa6
tareita <tareitaa@outlook.com>
2022-08-08 23:33:30 +0100

    user profile comments now has a link to post

--- client/src/components/UserComment.js
+++ client/src/components/UserComment.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserComment = (props) => {
   const { comment } = props;
+  const post = comment.post;
   return (
     <div>
       <div className="card mb-4">
         <div className="card-body">
-          <h5 className="card-title">{comment.post.title}</h5>
+          <h5 className="card-title">
+            <Link to={"/posts/" + post.topic.name + "/" + post._id}>
+              {post.title}
+            </Link>
+          </h5>
           <h6 className="card-subtitle mb-2 text-muted">
             {comment.author.username}
           </h6>
