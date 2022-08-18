cf89a084d1e73430d981eb2e331ac05982058e0a
tareita <tareitaa@outlook.com>
2022-08-18 16:23:52 +0100

    dropdown for mobile

--- client/src/components/MobileNav.js
+++ client/src/components/MobileNav.js
@@ -40,7 +40,31 @@ const MobileNav = (props) => {
           </div>
         </div>
       ) : (
-        <div></div>
+        <div>
+          <div class="btn-group dropstart">
+            <button
+              type="button"
+              class="btn btn-secondary dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+              style={{ color: "var(--bs-primary)" }}
+            >
+              ...
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <Link to="/register" className="dropdown-item">
+                  <i className="fa-solid fa-right-to-bracket" /> Register
+                </Link>{" "}
+              </li>
+              <li>
+                <Link to="/login" className="dropdown-item">
+                  <i className="fa-regular fa-user" /> Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       )}
     </div>
   );
--- client/src/components/Topics.js
+++ client/src/components/Topics.js
@@ -13,13 +13,13 @@ const Topics = () => {
       description: "Talk about the latest chapter releases here",
     },
     {
-      topic: "Movies",
-      description: "Ayo, new Makoto Shinkai movie just dropped?",
-    },
-    {
       topic: "Games",
       description:
         "Chat about games here (please no doki doki literature club)",
+    },
+    {
+      topic: "Movies",
+      description: "Ayo, new Makoto Shinkai movie just dropped?",
     },
     { topic: "Music", description: "Post your favourite J-Pop songs" },
     { topic: "Off-topic", description: "Anything non-anime related goes here" },
